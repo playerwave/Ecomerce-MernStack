ae82e8a4ee5e32315fe3f6400a28cadb77e6b1f7
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-11-11 18:55:08 +0700

    Database setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,4 +13,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+
+  connectDB();
 });
