a467735763bc3f52db2e6635c2944aaae76bc3af
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-11-11 18:30:55 +0700

    backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res, next) => {
+  res.send("Sign up route called")
+}
+
+export const login = async (req, res, next) => {
+    res.send("Login route called")
+  }
+
+export const logout = async (req, res, next) => {
+    res.send("Logout route called")
+  }
