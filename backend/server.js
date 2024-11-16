d773f15d771e2614024dafc7efd582c0ebd3f448
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-11-16 14:33:40 +0700

    Auth, Refresh & Access Tokens

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -8,6 +9,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
